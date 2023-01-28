bcbd7ff462612e5f8e8a603af1172e1ded366bd9
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2023-01-28 15:55:24 +0100

    [FT] Less work: now the server sends all data formatted

--- src/APIs/fetchData.js
+++ src/APIs/fetchData.js
@@ -38,52 +38,5 @@ export const getData = async (segments) => {
     .then(json=> json.json()) 
     .then(_data => _data)
 
-  console.log(data)
-
-  data.content.forEach(entry => {
-    //for dirs and files
-    entry.created_on = dateFormatter(entry.birthday, 'date')
-    entry.at = dateFormatter(entry.birthday, 'time')
-    
-    //only files
-    if (!entry.dir) {
-      entry.size = sizeFormatter(entry.size)
-    }
-
-    delete entry.birthday
-  })
-
   return data
-}
-
-function sizeFormatter(bytes) {
-    // converts bytes to human readable format
-    const sizes = ['KB', 'MB', 'GB', 'TB']; 
-
-    if (bytes < 1000) return `${bytes} bytes`
-
-    for(let i = 0; i < sizes.length; i++) {
-      bytes = bytes / 1024
-      if(bytes < 700) return `${bytes.toFixed(1)} ${sizes[i]}`
-    }
-}
-
-function dateFormatter(timestamp, type) {
-    if(timestamp === '-') return '-' // to handle the .. back directory
-
-    // converts timestamp to human readable format
-    const date = new Date(timestamp); 
-
-    if(type === 'date') {
-        const day = date.getDate().toString()
-        const month = (date.getMonth() + 1).toString()
-        const year = date.getFullYear();
-
-        return `${day}-${month}-${year}`;
-    } 
-
-    // for times
-    const hour = date.getHours().toString()
-    const minutes = date.getMinutes().toString()
-    return `${hour}:${minutes.length < 2 ? '0'+minutes : minutes}`;
 }
