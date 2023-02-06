54ec14b84e57fd0da0fa094dadfbfd44330c8025
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2023-02-06 21:50:11 +0100

    [FT] Upload file: Now the app can upload files to the server

--- src/components/dropFilesModal/dropFileModal.jsx
+++ src/components/dropFilesModal/dropFileModal.jsx
@@ -13,12 +13,16 @@ import {
   SelectButton
 } from './style.js'
 
+import { config } from '../../config.js'
 import { WorkspaceContext } from '../../context/workspaceContext.js'
+import {fetchData} from '../../APIs/fetchData.js'
 
 export const DropFilesModal = ({setShowDropFiles}) => {
-  const {workspace} = useContext(WorkspaceContext)
+  const {PORT, PROTOCOL, HOSTNAME} = config
+
+  const {workspace, updateWorkspace} = useContext(WorkspaceContext)
   const [dragActive, setDragActive] = useState(false)
-  const [files, setFiles] = useState([])
+  const [file, setFile] = useState()
 
   const input = useRef(null)
 
@@ -43,17 +47,19 @@ export const DropFilesModal = ({setShowDropFiles}) => {
     e.preventDefault();
     setDragActive(false);
 
-    if (e.dataTransfer.files && e.dataTransfer.files[0]) {  
-      setFiles(prev => [...prev, ...e.dataTransfer.files]); 
+    if (e.dataTransfer.files) {  
+      console.log(e.dataTransfer.files)
+      setFile(e.dataTransfer.files[0]); 
     }
   };
 
   // triggers when file is selected with click
   function handleChange(e) {
     e.preventDefault();
-    if (e.target.files && e.target.files[0]) {
+
+    if (e.target.files) {
       console.log(e.target.files)
-      setFiles(prev => [...prev, ...e.target.files]) 
+      setFile(e.target.files[0]); 
     }
   };
     
@@ -63,31 +69,37 @@ export const DropFilesModal = ({setShowDropFiles}) => {
     input.current.click()
   }
 
+  // send file to server
   async function sendData(e) {
     e.preventDefault()
 
-    await fetch(`http://192.168.0.13:3000/upload`, {
+    const formData = new FormData()
+
+    file && formData.append('file', file)
+
+    await fetch(`${PROTOCOL}://${HOSTNAME}:${PORT}/upload`, {
       method: 'POST',
-      body: files,
-      headers: {
-        'Content-Type': 'multipart/form-data',
-      }
+      body: formData
     })
       .then(res => res.json())
-      .then(res => console.log(res))
+      .then(res => {
+        console.log(res)
+        fetchData(workspace, updateWorkspace) // fetch workspace data again to update the files list
+        setShowDropFiles(false) // close modal
+      })
   }
 
   return (
     <ModalContainer onClick={closeModal}>
       <Modal onClick={e => e.stopPropagation()}>
-        <Form onDragEnter={handleDrag} onSubmit={(e) => e.preventDefault()}>
-          <Input type="file" name="file" multiple={true} onChange={handleChange} ref={input}/>
+        <Form onDragEnter={handleDrag} onSubmit={sendData}>
+          <Input type="file" name="file" onChange={handleChange} ref={input}/>
           <Title>Upload on</Title>
           <Text>/files/{workspace.path}</Text>
           <DragZone 
             className={dragActive ? 'dropActive' : ''} 
             onDragEnter={handleDrag} 
-            onDragOver={handleDrag} 
+            onDragOver={handleDrag} // necessary with html5 to handle drop event
             onDragLeave={handleDrag} 
             onDrop={handleDrop}
             >
@@ -98,15 +110,12 @@ export const DropFilesModal = ({setShowDropFiles}) => {
               <path d="M268.54,289H231.46a10.11,10.11,0,0,1-10.11-10.11V221.11A10.11,10.11,0,0,1,231.46,211h27.15a10.12,10.12,0,0,1,7.39,3.21l9.92,10.61a10.11,10.11,0,0,1,2.73,6.9v47.17A10.11,10.11,0,0,1,268.54,289Z" transform="translate(-221.35 -211)"/>
             </File>
           </DragZone>
+          <button>send</button>
         </Form>
 
         {
-          files[0] && files.map(file => {
-            return <p>{file.name}</p>
-          })
+          file && <p>{file.name}</p>
         }
-
-        <button onClick={sendData}>send</button>
       </Modal>
     </ModalContainer>
   )
