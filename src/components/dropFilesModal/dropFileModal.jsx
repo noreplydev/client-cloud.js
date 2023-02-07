4554dbea35c420b6595b135aff840738de291898
Cristian Sánchez <contact.cristiansanchez@gmail.com>
2023-02-07 23:50:03 +0100

    [FIX] Modal send button: now the modal send button has onClick function instead of onSubmit function  on form

--- src/components/dropFilesModal/dropFileModal.jsx
+++ src/components/dropFilesModal/dropFileModal.jsx
@@ -125,9 +125,8 @@ export const DropFilesModal = ({setShowDropFiles}) => {
                     <path d="M268.54,289H231.46a10.11,10.11,0,0,1-10.11-10.11V221.11A10.11,10.11,0,0,1,231.46,211h27.15a10.12,10.12,0,0,1,7.39,3.21l9.92,10.61a10.11,10.11,0,0,1,2.73,6.9v47.17A10.11,10.11,0,0,1,268.54,289Z" transform="translate(-221.35 -211)"/>
                   </File>
                 </DragZone>
-                <SelectButton>send</SelectButton>
+                <SelectButton onClick={sendData}>send</SelectButton>
               </>
-            
           }
         </Form>
       </Modal>
